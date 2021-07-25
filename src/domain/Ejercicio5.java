05d1867b370e8cb8a61dbe08d938087d7e83bc99
Federico Nicolas Rosales Cabrera <ferosales@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-07-25 12:48:52 -0300

    ejercicio 5

--- src/domain/Ejercicio5.java
+++ src/domain/Ejercicio5.java
@@ -2,25 +2,33 @@ package domain;
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        String personas[] = new String[3];
+        String personas[] = new String[4];
         personas[0] = "Federico";
         personas[1] = "Pedro";
         personas[2] = "Juan";
+        personas[3] = "Juancastro";
 
         maxLength(personas);
 
     }
 
-    public static void maxLength(String arr[]){
-            int max,min;
-            for(int i=0;i<arr.length;i++){
-                String nombre = arr[i];
-                int cont = 0;
-                for(int j=i;j < nombre.length(); j++){
-                    cont++;
-                }
-
+    public static void maxLength(String arr[]) {
+        int max = 0, min = 0, cont = 0;
+        for (int i = 0; i < arr.length; i++) {
+            String nombre = arr[i];
+            for (int j = i; j < nombre.length(); j++) {
+                cont++;
             }
-
+            if (cont < min) {
+                min = cont;
+                cont = 0;
+            } else if (cont > max) {
+                max = cont;
+                cont = 0;
+            } else {
+                min = cont;
+            }
+        }
+        System.out.println("max = " + max);
     }
 }
